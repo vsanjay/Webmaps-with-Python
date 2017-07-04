d499d8ceca16791ce22e60ca12629ebe6bb3a5a1
verdu sanjay <vsanjayvyas@gmail.com>
2017-07-04 13:05:05 +0530

    Created  a simple webmap interface

---
+++ webmap.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  4 12:37:44 2017
+
+@author: verdusanjay
+"""
+
+import folium as fol
+
+
+map = fol.Map([17.4479,78.3772])
+
+
+map.save('pyhtonmap.html')
