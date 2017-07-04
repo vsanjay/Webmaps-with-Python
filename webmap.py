f7d89961a77cd493dd58307d3e234a750a9e3226
verdu sanjay <vsanjayvyas@gmail.com>
2017-07-04 17:57:16 +0530

    Located all airports across world from
    'global_airports.csv' file

--- webmap.py
+++ webmap.py
@@ -7,13 +7,23 @@ Created on Tue Jul  4 12:37:44 2017
 """
 
 import folium as fol
+import pandas as pd
 
+airports_data = pd.read_csv("global_airports.csv")
+
+latitude_list = list(airports_data["latitude"])
+
+longitude_list = list(airports_data["longitude"])
+
+name_list = list(airports_data["name"])
 
 map = fol.Map([23.5509,87.2904])
 
 fg = fol.FeatureGroup()
 
-fg.add_child(fol.Marker([23.5509,87.2904],popup = "NIT DURGAPUR",icon = fol.Icon(color = 'blue')))
+for x in range(0,len(latitude_list)):
+
+    fg.add_child(fol.Marker([latitude_list[x],longitude_list[x]],popup = name_list[x],icon = fol.Icon(color = 'blue')))
 
 map.add_child(fg)
 
